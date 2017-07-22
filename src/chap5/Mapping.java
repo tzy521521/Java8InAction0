0cc8a52d3da0ec67a789c12b5fea349b4547ef54
“tzy521521 <843460950@qq.com>
2017-07-22 13:40:01 +0800

    chap5.3

--- src/chap5/Mapping.java
+++ src/chap5/Mapping.java
@@ -13,17 +13,20 @@ import java.util.stream.Stream;
  */
 public class Mapping {
     public static void main(String[] args) {
+        //map() 对流中的每一个元素应用函数
         List<String> dishNames = Dish.menu.stream()
                 .map(Dish::getName)
                 .collect(Collectors.toList());
         System.out.println(dishNames);
 
+        //map() 对流中的每一个元素应用函数
         List<String> words = Arrays.asList("Hello", "World");
         List<Integer> wordLengths = words.stream()
                 .map(String::length)
                 .collect(Collectors.toList());
         System.out.println(wordLengths);
 
+        //map() 对流中的每一个元素应用函数
         List<Integer> dishNamesLengths = Dish.menu.stream()
                 .map(Dish::getName)
                 .map(String::length)
@@ -32,31 +35,31 @@ public class Mapping {
 
         //流的扁平化5.2.2
         List<String[]> result = words.stream()
-                .map((String word)->word.split(""))
-                .distinct().collect(Collectors.toList());
-                //.forEach(System.out::println);
+                .map((String word)->word.split(""))//返回流实际上是Stream<String[]>
+                .distinct()
+                .collect(Collectors.toList());
         for (String[] temp:result) {
             for (int i = 0; i <temp.length ; i++) {
                 System.out.print(temp[i]+" ");
             }
             System.out.println();
         }
-  
+
         List<Stream<String>> result1 = words.stream()
                 .map((String word)->word.split(""))
                 .map(Arrays::stream)
-                .distinct().collect(Collectors.toList());
-                //.forEach(System.out::println);
+                .distinct()
+                .collect(Collectors.toList());
         for (Stream<String> temp:result1) {
-            temp.forEach((String s)->System.out.print(s+"/"));
+            temp.forEach((String s)->System.out.print(s+" "));
             System.out.println();
         }
 
-        Stream<String> result2 = words.stream()
+        List<String> result2 = words.stream()
                 .map((String word)->word.split(""))
                 .flatMap(Arrays::stream)
-                .distinct();
-                //.forEach(System.out::println);
-        result2.forEach((String s)-> System.out.print(s+"/"));
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println(result2);
     }
 }
