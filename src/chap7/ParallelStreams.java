8fb2bcab8aaee726bac2c7871be09716799589c5
lp <843460950@qq.com>
2017-07-24 02:03:16 +0800

    chap7.1

--- src/chap7/ParallelStreams.java
+++ src/chap7/ParallelStreams.java
@@ -18,30 +18,57 @@ public class ParallelStreams {
 
     //
     public static long sequentialSum(long n){
+        //拆箱装箱操作
         return Stream.iterate(1L,(Long i)->i+1).limit(n).reduce(0L,Long::sum);
     }
     public static long sequentialSum0(long n){
         return Stream.iterate(1L,(Long i)->i+1).limit(n).reduce(Long::sum).get();
     }
+
     //
     public static long parallelSum(long n) {
+        return Stream.iterate(1L, i -> i + 1).limit(n).parallel().reduce(0L,Long::sum);
+    }
+    public static long parallelSum0(long n) {
         return Stream.iterate(1L, i -> i + 1).limit(n).parallel().reduce(Long::sum).get();
     }
 
+
     //
     public static long rangedSum(long n) {
+        return LongStream.rangeClosed(1, n).reduce(0L,Long::sum);
+    }
+    public static long rangedSum0(long n) {
         return LongStream.rangeClosed(1, n).reduce(Long::sum).getAsLong();
     }
 
-    public static long rangedSum1(long n) {
-        return LongStream.rangeClosed(1, n).reduce(0L,Long::sum);
-    }
-
+    //
     public static long parallelRangedSum(long n) {
+        return LongStream.rangeClosed(1, n).parallel().reduce(0L,Long::sum);
+    }
+    public static long parallelRangedSum0(long n) {
         return LongStream.rangeClosed(1, n).parallel().reduce(Long::sum).getAsLong();
     }
 
-    public static long parallelRangedSum1(long n) {
-        return LongStream.rangeClosed(1, n).parallel().reduce(0L,Long::sum);
+    //
+    public static long sideEffectSum(long n) {
+        Accumulator accumulator = new Accumulator();
+        LongStream.rangeClosed(1, n).forEach(accumulator::add);
+        return accumulator.total;
     }
+
+    public static long sideEffectParallelSum(long n) {
+        Accumulator accumulator = new Accumulator();
+        LongStream.rangeClosed(1, n).parallel().forEach(accumulator::add);
+        return accumulator.total;
+    }
+
+    public static class Accumulator {
+        private long total = 0;
+
+        public void add(long value) {
+            total += value;
+        }
+    }
+
 }
