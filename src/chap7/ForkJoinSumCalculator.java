2a90eeaf99b192c4d447d192ad3c13ead16f4222
“tzy521521 <843460950@qq.com>
2017-07-24 10:19:22 +0800

    chap7.2

---
+++ src/chap7/ForkJoinSumCalculator.java
@@ -0,0 +1,53 @@
+package chap7;
+
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
+import java.util.stream.LongStream;
+
+import static chap7.ParallelStreamsHarness.FORK_JOIN_POOL;
+
+/**
+ * Created by tzy on 2017/7/24.
+ */
+public class ForkJoinSumCalculator extends RecursiveTask<Long>{
+    public static final long THRESHOLD = 10_000;
+    private final long[] numbers;
+    private final int start;
+    private final int end;
+
+    public ForkJoinSumCalculator(long[] numbers) {
+        this(numbers, 0, numbers.length);
+    }
+    private ForkJoinSumCalculator(long[] numbers, int start, int end) {
+        this.numbers = numbers;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Long compute() {
+        int length = end - start;
+        if (length <= THRESHOLD) {
+            return computeSequentially();
+        }
+        ForkJoinSumCalculator leftTask = new ForkJoinSumCalculator(numbers, start, start + length/2);
+        leftTask.fork();
+        ForkJoinSumCalculator rightTask = new ForkJoinSumCalculator(numbers, start + length/2, end);
+        Long rightResult = rightTask.compute();
+        Long leftResult = leftTask.join();
+        return leftResult + rightResult;
+    }
+    //在子任务不再可分时计算结果的简单算法
+    private long computeSequentially() {
+        long sum = 0;
+        for (int i = start; i < end; i++) {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+    public static long forkJoinSum(long n) {
+        long[] numbers = LongStream.rangeClosed(1, n).toArray();
+        ForkJoinTask<Long> task = new ForkJoinSumCalculator(numbers);
+        return FORK_JOIN_POOL.invoke(task);
+    }
+}
--- src/chap7/ParallelStreamsHarness.java
+++ src/chap7/ParallelStreamsHarness.java
@@ -1,33 +1,60 @@
 package chap7;
 
+import java.util.concurrent.ForkJoinPool;
 import java.util.function.Function;
 
 /**
  * Created by tzy on 2017/7/23.
  */
 public class ParallelStreamsHarness {
-    public static void main(String[] args) {
-        //System.out.println("Iterative Sum done in: " + measurePerf(ParallelStreams::iterativeSum, 10_000_000L) + " msecs");
-        //System.out.println("Sequential Sum done in: " + measurePerf(ParallelStreams::sequentialSum, 10_000_000L) + " msecs");
-        //System.out.println("Parallel forkJoinSum done in: " + measurePerf(ParallelStreams::parallelSum, 10_000_000L) + " msecs" );
-        //System.out.println("rangedSum Sum done in: " + measurePerf(ParallelStreams::rangedSum, 10_000_000L) + " msecs");
-        //System.out.println("rangedSum Sum done in: " + measurePerf(ParallelStreams::parallelRangedSum, 10_000_000L) + " msecs");
 
+    public static final ForkJoinPool FORK_JOIN_POOL = new ForkJoinPool();
+
+    public static void main(String[] args) {
         //用传统for循环的迭代版本执行：更底层，不需要对原始类型做装箱操作。
+        /*
+        System.out.println("Iterative Sum done in: " + measurePerf(
+                (Long n)->{
+                    long result = 0;
+                    for (long i = 0; i <= n; i++) {
+                        result += i;
+                    }
+                    return result;
+                }
+                , 10_000_000L) + " msecs"
+        );
+         */
         System.out.println("Iterative Sum done in: " + measurePerf(ParallelStreams::iterativeSum, 10_000_000L) + " msecs");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~");
         System.out.println("Sequential Sum done in: " + measurePerf(ParallelStreams::sequentialSum, 10_000_000L) + " msecs");
         System.out.println("Sequential0 Sum done in: " + measurePerf(ParallelStreams::sequentialSum0, 10_000_000L) + " msecs");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("Parallel forkJoinSum done in: " + measurePerf(ParallelStreams::parallelSum, 10_000_000L) + " msecs" );
+        System.out.println("Parallel forkJoinSum done in: " + measurePerf(ParallelStreams::parallelSum0, 10_000_000L) + " msecs" );
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("Range forkJoinSum done in: " + measurePerf(ParallelStreams::rangedSum, 10_000_000L) + " msecs" );
+        System.out.println("Range forkJoinSum done in: " + measurePerf(ParallelStreams::rangedSum0, 10_000_000L) + " msecs" );
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("ParallelRange forkJoinSum done in: " + measurePerf(ParallelStreams::parallelRangedSum, 10_000_000L) + " msecs" );
+        System.out.println("ParallelRange forkJoinSum done in: " + measurePerf(ParallelStreams::parallelRangedSum0, 10_000_000L) + " msecs" );
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("SideEffect sum done in: " + measurePerf(ParallelStreams::sideEffectSum, 10_000_000L) + " msecs" );
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("SideEffect prallel sum done in: " + measurePerf(ParallelStreams::sideEffectParallelSum, 10_000_000L) + " msecs" );
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("ForkJoin sum done in: " + measurePerf(ForkJoinSumCalculator::forkJoinSum, 10_000_000L) + " msecs" );
     }
     public static  <T,R>long measurePerf(Function<T,R> adder,T n){
         long fastest = Long.MAX_VALUE;
+        Object sum=null;
         for (int i = 0; i <5 ; i++) {
             long start = System.nanoTime();
-            R sum =adder.apply(n);
+            sum =adder.apply(n);
             long duration = (System.nanoTime() - start) / 1_000_000;
-            //System.out.print("Result: " + sum+"/");
-            System.out.print("Time: " + duration+"/");
+            System.out.print("Time: " + duration+"/ ");
             if (duration < fastest) fastest = duration;
         }
+        System.out.print("Result: " +sum+" ");
         return fastest;
     }
 }
